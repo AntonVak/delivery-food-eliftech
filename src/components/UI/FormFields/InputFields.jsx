59d8bab984b7a5ba9f5d098f0d14bbd5502946cd
AntonVak <antonv.snab@gmail.com>
2023-08-31 20:27:48 +0300

    add input field

---
+++ src/components/UI/FormFields/InputFields.jsx
@@ -0,0 +1,27 @@
+import { useController } from "react-hook-form";
+import { TextField } from "@mui/material";
+
+const InputField = ({ label, name, control, type }) => {
+  const {
+    field: { ref, onBlur, onChange },
+    fieldState: { invalid, error },
+  } = useController({
+    name,
+    control,
+  });
+  return (
+    <>
+      <TextField
+        id="outlined-basic"
+        label={label}
+        onBlur={onBlur}
+        onChange={onChange}
+        ref={ref}
+        type={type}
+      />
+      {invalid && <p>{error?.message}</p>}
+    </>
+  );
+};
+
+export default InputField;
