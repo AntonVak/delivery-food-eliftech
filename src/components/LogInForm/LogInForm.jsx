d91e447b5b8ac52d01f205ed78fdc51d913c4eb1
AntonVak <antonv.snab@gmail.com>
2023-08-31 20:29:01 +0300

    add Comp LoginForm

---
+++ src/components/LogInForm/LogInForm.jsx
@@ -0,0 +1,31 @@
+import { Typography } from "@mui/material";
+import { useForm } from "react-hook-form";
+import InputField from "../UI/FormFields/InputFields";
+
+const RegisterForm = () => {
+  const { handleSubmit, reset, control } = useForm({
+    defaultValues: {
+      firstName: "",
+      email: "",
+      password: "",
+    },
+    mode: "onTouched",
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+    // reset();
+  };
+  return (
+    <>
+      <Typography variant="h4" component="h2" mt={2}>
+        Register form
+      </Typography>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <InputField control={control} name="name" label="Name" />
+      </form>
+    </>
+  );
+};
+
+export default RegisterForm;
